da865d44abe7a2af01a3ab5025020afa6289f76c
Adam Napoletano <adam.napps@gmail.com>
2020-10-16 14:51:24 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+ first_value = hash.first[1]
+  name_hash.each do |item, value|
+  
+    if value < first_value 
+      lowest_item = item
+    end
+  end
+  lowest_item
 end
