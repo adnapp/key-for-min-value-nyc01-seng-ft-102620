a467cb265da9cd6b73ded58d8ea3b15231899120
Adam Napoletano <adam.napps@gmail.com>
2020-10-16 14:52:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
  first_value = name_hash.first[1]
   name_hash.each do |item, value|
-
+    lowest_item = ""
     if value < first_value
       lowest_item = item
     end
